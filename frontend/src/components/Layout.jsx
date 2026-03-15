91f298133990705d9599811c3291ad361ed1f29c
madhanrajdeveloper <madhanraj.developer@gmail.com>
2026-03-16 01:48:58 +0530

    fix: adjust logo image dimensions for better display in layout

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -19,7 +19,7 @@ export default function Layout() {
             {/* Sidebar */}
             <aside className="w-64 bg-slate-900 text-white flex flex-col">
                 <div className="p-6 text-2xl font-bold border-b border-slate-800 text-yellow-400 flex gap-x-4 items-center align-middle">
-                     <img src={logo} alt="Logo" />
+                     <img src={logo} alt="Logo" width='200px' height='200px' />
                      <p>Pawn Shop</p>
                 </div>
                 <nav className="flex-1 p-4 space-y-2">
